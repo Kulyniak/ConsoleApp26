6cda5b8015669a8e02151df5f2f519567edad139
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-07-17 23:04:41 +0300

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var List = new List<KeyValuePair<string, int>>();
+            List.Add(new KeyValuePair<string, int>("Bob", 27));
+            foreach (var item in List)
+            {
+                Console.WriteLine(item);
+            }
+            string Foo = List.ToString();
+            Console.WriteLine( Foo);
+            var Boys  = new List<string>();
+            Boys.Add("vasyl, oleg, victor, dmitro");
+            Boys.ForEach(Console.WriteLine);
+        }
+
+       
+    }
+}
